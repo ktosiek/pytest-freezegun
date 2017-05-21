9c9df82ab68d07fdc1a7a34967d65ce0a96f3b68
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2017-05-21 17:13:45 +0200

    Access to the freezer in tests

--- pytest_freezegun.py
+++ pytest_freezegun.py
@@ -4,9 +4,15 @@ import pytest
 from freezegun import freeze_time
 
 
-def pytest_runtest_setup(item):
-    marker = item.get_marker("freeze_time")
-    if marker is not None:
-        freezer = freeze_time(*marker.args, **marker.kwargs)
-        freezer.start()
-        item.addfinalizer(freezer.stop)
+@pytest.yield_fixture
+def freezer(request):
+    marker = request.node.get_marker('freeze_time')
+    args, kwargs = (marker.args, marker.kwargs) if marker else ((), {})
+    with freeze_time(*marker.args, **marker.kwargs) as freezer:
+        yield freezer
+
+
+@pytest.fixture(autouse=True)
+def _auto_freezer(request):
+    if request.node.get_marker('freeze_time'):
+        request.getfixturevalue('freezer')
--- tests/test_freezegun.py
+++ tests/test_freezegun.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
 
 def test_freezing_time(testdir):
     testdir.makepyfile("""
@@ -8,6 +10,35 @@ def test_freezing_time(testdir):
         @pytest.mark.freeze_time('2017-05-20 15:42')
         def test_sth():
             assert datetime.now().date() == date(2017, 5, 20)
+    """)
+
+    result = testdir.runpytest('-v', '-s')
+    assert result.ret == 0
+
+
+def test_no_mark(testdir):
+    testdir.makepyfile("""
+        import datetime
+
+        def test_sth():
+            assert datetime.datetime.now() > {}
+    """.format(repr(datetime.now())))
+
+    result = testdir.runpytest('-v', '-s')
+    assert result.ret == 0
+
+
+def test_move_to(testdir):
+    testdir.makepyfile("""
+        from datetime import date
+        import pytest
+
+        @pytest.mark.freeze_time
+        def test_changing_date(freezer):
+            freezer.move_to('2017-05-20')
+            assert date.today() == date(2017, 5, 20)
+            freezer.move_to('2017-05-21')
+            assert date.today() == date(2017, 5, 21)
     """)
 
     result = testdir.runpytest('-v', '-s')
