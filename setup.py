8a1f0ac470b260850f558b5fbf9f63425d1f4db4
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2017-09-25 20:47:13 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-freezegun',
-    version='0.1.0',
+    version='0.2.0',
     author='Tomasz Kontusz',
     author_email='tomasz.kontusz@gmail.com',
     maintainer='Tomasz Kontusz',
